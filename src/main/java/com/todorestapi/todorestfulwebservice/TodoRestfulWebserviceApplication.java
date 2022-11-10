39093199a6128885cfe7dffc980b9e78dbe40144
Lavan <lavanya.b@seaant.com>
2022-11-10 13:35:58 +0530

    initial commit

---
+++ src/main/java/com/todorestapi/todorestfulwebservice/TodoRestfulWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.todorestapi.todorestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestfulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRestfulWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todorestapi/todorestfulwebservice/TodoRestfulWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todorestapi.todorestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestfulWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
